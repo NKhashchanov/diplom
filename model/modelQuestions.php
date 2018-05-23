1eba625067ee6fb5fac0977f61b73f3d2de2d879
Николай <37711307+NKhashchanov@users.noreply.github.com>
2018-05-23 17:16:53 +0300

    delete wrong comments

--- model/modelQuestions.php
+++ model/modelQuestions.php
@@ -162,60 +162,7 @@ class ModelQuestions
         $stmt->bindParam(':themeID', $themeID);
         return $stmt->execute();
     }
-    /*
-// Логин пользователя
-    function login($params)
-    {
-        global $pdo;
-        $login = $params['login'];
-        $password = $params['password'];
-        $stmt = $pdo->prepare('SELECT * FROM admins WHERE login = :login AND password = :password');
-        $stmt->bindParam(':login', $login);
-        $stmt->bindParam(':password', $password);
-        $stmt->execute();
-        foreach ($stmt->fetchAll() as $data) {
-             $_SESSION['login'] = $data['login'];
-             $_SESSION['id'] = $data['id'];
-        }
-    }
 
-// Изменить пароль администратора
-    function edit($params)
-    {
-        global $pdo;
-        $id = $params['id'];
-        $edit = $params['password'];
-        $stmt = $pdo->prepare('UPDATE admins SET password = :password WHERE id = :id');
-        $stmt->bindParam(':password', $edit);
-        $stmt->bindParam(':id', $id);
-        return $stmt->execute();
-    }
-
-// Удалить администратора
-    function delete($params)
-    {
-        global $pdo;
-        $id = $params['id'];
-        $stmt = $pdo->prepare('DELETE FROM admins WHERE id = :id');
-        $stmt->bindParam(':id', $id);
-        return $stmt->execute();
-    }
-
-// Добавить администратора
-    function add($params)
-    {
-        global $pdo;
-        $login = $params['login'];
-        $password = $params['password'];
-        $id = null;
-        $stmt = $pdo->prepare('INSERT INTO admins (id, login, password) VALUES(?, ?, ?)');
-        $stmt->bindParam(1, $id);
-        $stmt->bindParam(2, $login);
-        $stmt->bindParam(3, $password);
-        return $stmt->execute();
-    }
-
-*/
 }
 
 ?>
